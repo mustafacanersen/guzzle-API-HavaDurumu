ca92b037bf31a5cd43cb1363833f404b4bab3491
Mustafa Can Ersen <mustafa.can.ersen@babursahturizm.com>
2023-08-11 10:33:40 +0300

    veriler çekildi ve front-end özellikleri eklendi

--- anasayfa.view.php
+++ anasayfa.view.php
@@ -6,17 +6,43 @@
     <title>Document</title>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js" type="text/javascript""></script>
     <script>
-    $(document).ready(function(){
+    function getCurrentWeather(){
         $.ajax({
-            url: 'localhost/guzzle-API-HavaDurumu/',
+            url: '',
             type: 'POST',
             error: function(xhr, status, error) {
                 console.error('API isteği sırasında hata oluştu. Hata: ' + error);
+            },
+            success: function(result){
+               $('body').empty().append(result);
             }
         });
-        setInterval(1200000);
     }
+    $(document).ready(function(){
+        getCurrentWeather();
+        setInterval(getCurrentWeather, 1200000);
+    })
     </script>
+    <style>
+        html{
+            height: 100%;
+            width: 100%;
+            margin: 0;
+            display:flex;
+        }
+        body{
+            height: 100%;
+            width: 100%;
+            margin: 0;
+            display:flex;
+            background-image:url(images/background.jpg);
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+
+        }
+    </style>
 </head>
 <body>
     
--- index.php
+++ index.php
@@ -7,22 +7,63 @@ $router->setBasePath('/guzzle-API-HavaDurumu');
 
 $router->map('GET','/',function(){
     require_once __DIR__.'/anasayfa.view.php';
-    
 });
 
 $router->map('POST','/', function(){
-  
-    $client = new Client([
-        'base_uri' => 'https://collectapi.com/tr/api/',
-        'headers' => 'apikey 5aF3J9rP2Luj8gsVj28Lwh:4hlGK7uTkRMQ99IcdQsJqO'
-    ]);
+    
     try{
-        $response = $client->request('GET', 'weather/hava-durumu-api');
-        $responseData = json_decode($response->getBody(), true);
-        print_r($responseData);
-    } catch (Exception $e) {
+        $client = new Client([
+            'base_uri'  => 'https://api.collectapi.com/',
+            'headers'   => [
+                'authorization' => 'apikey 5aF3J9rP2Luj8gsVj28Lwh:4hlGK7uTkRMQ99IcdQsJqO',
+                'content-type'  => 'application/json'
+            ]
+        ]);
+        $response = $client->request('GET', 'weather/getWeather?data.lang=tr&data.city=izmir');
+        $response = $response->getBody();
+        $result = json_decode($response, true);
+
+        if (isset($result['result']) && is_array($result['result'])) {
+            echo "<div class='weather-container' style='display:flex;width: 100%;align-items: center;justify-content: center;justify-content: space-around;'>";
+
+            foreach ($result['result'] as $weatherData) {
+                echo "<div class='card' style='border-style:solid; border-width: 1px; border-color: white;width: 200px;height: auto; display: flex; flex-direction: column; align-items: center;background-color: rgba(115, 228, 250, 0.3);box-shadow: 2px 2px 2px 2px white;border-radius: 5px 5px 5px 5px;'>
+                        <p>" . $weatherData['date'] . "</p>
+                        <p>" . ucfirst($result['city']) . "</p>
+                        <img src='" . $weatherData['icon'] . "' style='width: 100px; height: auto;'>
+                        <p style='font-size:x-large;'>" . $weatherData['degree'] . "</p>
+                        <p>" . $weatherData['day'] . "</p>
+                        <p>" . ucfirst($weatherData['description']) . "</p>
+                        <p>En Düşük: " . $weatherData['min'] . "</p>
+                        <p>En Yüksek: " . $weatherData['max'] . "</p>
+                    </div>";
+            }
+            echo "</div>";
+        } else {
+            echo "Hava durumu verisi yok";
+        }
+
+    }
+    catch (\Exception $e) {
         echo "API isteği sırasında bir hata oluştu: " . $e->getMessage();
     }
-    
-})
-?>
+    catch (\Throwable $th) {
+        echo "API isteği sırasında bir hata oluştu: " . $th->getMessage();
+    }
+    catch (GuzzleHttp\Exception $ge) {
+        echo "API isteği sırasında bir hata oluştu: " . $ge->getMessage();
+    }
+});
+
+$match = $router->match();
+
+if( is_array($match) && is_callable( $match['target'] ) ) {
+    call_user_func_array( $match['target'], $match['params'] ); 
+} else {
+    echo 'rota bulunamadı url: '.$_SERVER['REQUEST_URI'];
+    //header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+}
+
+// Problem 1: Route match yok dolayısıyla / rotası eşleşip anasayfa.view.php'yi çağırmıyor jslerin görüneceği sayfa hiç yüklenmiyor yani
+// dizin adından dolayı index.php default page olarak çalışıyor ama rota yapısı dışında kod barındırmadığı için de çıktı üretmiyor
+// Çözüm 1: Match kodlarını ekliyoruz bu alana ve rotanın eşleşmesini sağlıyoruz
